d652d6fa85053170b87f1047a85b011c746b9c3a
DevonJP <105997401+DevonJP@users.noreply.github.com>
2022-05-28 15:43:38 -0500

    Roberto  Janampa
    Semaforo controller Part 1

--- Assets/Scripts/SemaforoController.cs
+++ Assets/Scripts/SemaforoController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class SemaforoController : MonoBehaviour
 {
+    public SemaforoModel model;
+    public float TotalTime=75f;
+    
+    public float ChangeTime;
+    public float timer_done=0;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,66 @@ public class SemaforoController : MonoBehaviour
     void Update()
     {
         
+        DescRed();
+        
     }
+    public void DescRed()
+    {
+        float t = 40f;
+        ChangeTime = t;
+        timer_done += Time.deltaTime;
+        if (timer_done >= ChangeTime)
+
+        {
+            
+            model.luzroja.SetActive(false);
+           if(timer_done >= 75f)
+            {
+                model.luzroja.SetActive(true);
+                timer_done = 0;
+            }
+            
+        }
+        
+    }
+    public void DescAmar()
+    {
+        float t = 40f;
+        ChangeTime = t;
+        timer_done += Time.deltaTime;
+        if (timer_done >= ChangeTime)
+
+        {
+
+            model.luzamarilla.SetActive(false);
+            if (timer_done >= 35f)
+            {
+                model.luzamarilla.SetActive(true);
+                timer_done = 0;
+            }
+
+        }
+
+    }
+    public void DescVerd()
+    {
+        float t = 45f;
+        ChangeTime = t;
+        timer_done += Time.deltaTime;
+        if (timer_done >= ChangeTime)
+
+        {
+
+            model.luzverde.SetActive(false);
+            if (timer_done >= 30f)
+            {
+                model.luzverde.SetActive(true);
+                timer_done = 0;
+            }
+
+        }
+
+    }
+
+
 }
--- Assets/Scripts/SemaforoModel.cs
+++ Assets/Scripts/SemaforoModel.cs
@@ -9,11 +9,12 @@ public class SemaforoModel : MonoBehaviour
     public GameObject luzamarilla;
     public GameObject luzverde;
 
-    public float tiempobucle;
-    public float tiemporojo;
+    
+    public float tiemporojo = 40f;
     public float tiemporaa;
+    public float tiempoamar = 5f;
     public float tiempoaav;
-    public float tiempoverde;
+    public float tiempoverde = 30;
 
     // Start is called before the first frame update
 
